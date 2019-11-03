86c8f3233a59c1b6bd39bc85b47fabcef5e6cf40
Russ Lewis <russl5445@gmail.com>
2019-11-03 16:38:30 +0000

    Created the shell of the game and control planes for Ericonium

---
+++ server/ericonium_control_plane.py
@@ -0,0 +1,28 @@
+
+import json
+import MySQLdb
+
+import passwords
+
+
+
+from flask import Flask, request, render_template, url_for, redirect, make_response, jsonify
+app = Flask(__name__, template_folder="templates-control_plane")
+
+
+
+def get_db():
+    if "db_conn" not in dir(request):
+        request.db_conn = MySQLdb.connect(host   = passwords.SQL_HOST,
+                                          user   = passwords.SQL_USER,
+                                          passwd = passwords.SQL_PASSWD,
+                                          db     = "ericonium")
+    return request.db_conn
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
---
+++ server/ericonium_game_plane.wsgi
@@ -0,0 +1,14 @@
+import sys
+import os
+
+
+APP_DIR = "/home/ec2-user/ericonium/server/"
+
+# why doesn't WSGI set this up automatically?  I find the next two steps
+# fairly annoying!
+sys.path.insert(0, APP_DIR)
+os.chdir(APP_DIR)
+
+
+from ericonium_game_plane import app as application
+
